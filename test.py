7e7d53ff817585ccf5606d75ae267e5e39ad1a01
c0d3-br3ak9r <103585603+c0d3-br3ak9r@users.noreply.github.com>
2022-11-08 20:20:36 +0530

    Organized the project

--- test.py
+++ test.py
@@ -1,8 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return "<h2>Hello, World</h2>"
+    return '''<h2>Page under construction</h2>
+                <p>Goto <a href="/signup">Signup</a> Page</p>
+        '''
+
+@app.route('/signup')
+def signup_page():
+    return render_template("signup.html")
+
+@app.route('/login')
+def login_page():
+    return render_template("login.html")
+
+@app.route('/forgot-password')
+def forgot_password_page():
+    return render_template("forgot.html")
 
 app.run(debug=True)
